98bc5dd6ce1ec62df3a9d9eabddcaaf0d94eb5dd
Liirus91 <work.liirus@gmail.com>
2024-05-07 17:28:19 +0300

    feat: create fetchCars function

---
+++ utils/index.ts
@@ -0,0 +1,15 @@
+export async function fetchCars() {
+  const headers = {
+    'X-RapidAPI-Key': process.env.API_KEY || '',
+    'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com',
+  };
+
+  const responce = await fetch(
+    'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars',
+    { headers: headers }
+  );
+
+  const result = await responce.json();
+
+  return result;
+}
