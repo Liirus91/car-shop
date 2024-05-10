a3d6556dc0b8c039891ff213b9cb370979a7d7eb
Liirus91 <work.liirus@gmail.com>
2024-05-10 09:21:28 +0300

    feat: add generateCarImageUrl util

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,5 @@
+import { Car } from '@/types';
+
 export async function fetchCars() {
   const headers = {
     'X-RapidAPI-Key': process.env.API_KEY || '',
@@ -25,4 +27,19 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
   const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
 
   return rentalRatePerDay.toFixed(0);
+};
+
+export const generateCarImageUrl = (car: Car, angle?: string) => {
+  const url = new URL('https://cdn.imagin.studio/getimage');
+
+  const { make, year, model } = car;
+
+  url.searchParams.append('customer', process.env.IMAGE_API_KEY || '');
+  url.searchParams.append('make', make);
+  url.searchParams.append('moelFamily', model.split(' ')[0]);
+  url.searchParams.append('zoomType', 'fullscreen');
+  url.searchParams.append('modelYear', `${year}`);
+  url.searchParams.append('angle', `${angle}`);
+
+  return `${url}`;
 };
