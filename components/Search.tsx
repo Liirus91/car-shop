62151e788cb281554c2ccad3886c9e45db2c6948
Liirus91 <work.liirus@gmail.com>
2024-05-10 09:31:05 +0300

    feat: add SearchButton

--- components/Search.tsx
+++ components/Search.tsx
@@ -2,6 +2,19 @@
 
 import { useState } from 'react';
 import { SearchManufacturer } from '.';
+import Image from 'next/image';
+
+const SearchButton = () => (
+  <button type="submit" className="-ml-3 z-10 sm:hidden">
+    <Image
+      src="/magnifying-glass.svg"
+      alt="Magnifying glass"
+      width={40}
+      height={40}
+      className="object-contain"
+    />
+  </button>
+);
 
 const Search = () => {
   const [manufacturer, setManufacturer] = useState('');
@@ -14,6 +27,7 @@ const Search = () => {
           manufacturer={manufacturer}
           setManufacturer={setManufacturer}
         />
+        <SearchButton />
       </div>
     </form>
   );
