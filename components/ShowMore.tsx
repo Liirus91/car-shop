3010a47a5d34940012abc6eaca34cc854300d689
Liirus91 <work.liirus@gmail.com>
2024-05-12 13:51:27 +0300

    feat: save in url params from limits in page

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -1,5 +1,35 @@
-const ShowMore = () => {
-  return <div>ShowMore</div>;
+'use client';
+
+import { useRouter } from 'next/navigation';
+import CustomButton from './CustomButton';
+import { updateSearchParams } from '@/utils';
+
+interface ShowMoreProps {
+  pageNumber: number;
+  isNext: boolean;
+}
+
+const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
+  const router = useRouter();
+
+  const handleNavigation = () => {
+    const newLimit = (pageNumber + 1) * 10;
+    const newPath = updateSearchParams('limit', String(newLimit));
+
+    router.push(newPath);
+  };
+
+  return (
+    <div className="w-full flex-center gap-5 mt-10">
+      {!isNext && (
+        <CustomButton
+          title="Show more"
+          containerStyles="bg-primary-blue rounded-full text-white"
+          handleClick={handleNavigation}
+        />
+      )}
+    </div>
+  );
 };
 
 export default ShowMore;
